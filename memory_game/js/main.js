84c4a0a424342cf55a5b7c03357cedea3a1f2b7c
unknown <mgmurp02@gmail.com>
2020-03-20 15:35:45 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+let cardOne = "queen";
+
+let cardTwo = "queen";
+
+let cardThree = "king";
+
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
